56963ba28c4974366fe7f527bfebd5b19f6334a0
Ain Ehasalu <ainehasalu@gmail.com>
2017-06-13 20:44:25 +0300

    Added integers

--- src/ee/taps/koolitus/HelloWorld.java
+++ src/ee/taps/koolitus/HelloWorld.java
@@ -5,6 +5,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Tere spordimees, Ain!");
 
-	}
-
+	
+int a=5;
+int b=++a;
+System.out.println(b);
+System.out.println(a);
+int c=a++;
+System.out.println(c);
+System.out.println(a);
 }
+}	
